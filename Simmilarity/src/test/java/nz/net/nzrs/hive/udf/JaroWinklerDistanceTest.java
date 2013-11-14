14588837c59af1eaa38a0a9cf070031cfd906063
Sebastian Castro <sebastian@nzrs.net.nz>
2013-11-15 11:38:23 +1300

    Version of the simmilarity library that works.

--- Simmilarity/src/test/java/nz/net/nzrs/hive/udf/JaroWinklerDistanceTest.java
+++ Simmilarity/src/test/java/nz/net/nzrs/hive/udf/JaroWinklerDistanceTest.java
@@ -1,9 +1,9 @@
 package nz.net.nzrs.pig.udf;
 
-import junit.framework.Assert;
+// import junit.framework.Assert;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
-import org.junit.Test;
+// import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -14,9 +14,9 @@ import junit.framework.TestSuite;
 
 public class JaroWinklerDistanceTest extends TestCase {
     private TupleFactory tupleFactory = TupleFactory.getInstance();
-    final private Text text1 = new Text("home");
-    final private Text text2 = new Text("homer");
-    final private Text text3 = new Text("arthur");
+    final private String text1 = new String("home");
+    final private String text2 = new String("homer");
+    final private String text3 = new String("arthur");
     final private double epsilon = 0.0001;
 
     public void test1() throws IOException {
@@ -26,14 +26,14 @@ public class JaroWinklerDistanceTest extends TestCase {
         assertEquals(expected, res, epsilon);
     }
 
-    public void test2() {
+    public void test2() throws IOException {
         Tuple input = tupleFactory.newTuple(Arrays.asList(text2, text3));
         final double res = new JaroWinklerDistance().exec(input);
         final double expected = 0.0;
         assertEquals(expected, res, epsilon);
     }
 
-    public void test3() {
+    public void test3() throws IOException {
         Tuple input = tupleFactory.newTuple(Arrays.asList(text3, text3));
         final double res = new JaroWinklerDistance().exec(input);
         final double expected = 1.0;
--- Simmilarity/src/test/java/nz/net/nzrs/hive/udf/MongeElkanDistanceTest.java
+++ Simmilarity/src/test/java/nz/net/nzrs/hive/udf/MongeElkanDistanceTest.java
@@ -1,18 +1,24 @@
-package nz.net.nzrs.hive.udf;
+package nz.net.nzrs.pig.udf;
 
-import org.apache.hadoop.io.Text;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class MongeElkanDistanceTest extends TestCase {
-    final private Text text1 = new Text("home");
-    final private Text text2 = new Text("casa");
+    private TupleFactory tupleFactory = TupleFactory.getInstance();
+    final private String text1 = new String("home");
+    final private String text2 = new String("casa");
     final private double epsilon = 0.0001;
 
-    public void test1() {
-        final double res = new MongeElkanDistance().evaluate(text1, text2);
+    public void test1() throws IOException {
+        Tuple input = tupleFactory.newTuple(Arrays.asList(text1, text2));
+        final double res = new MongeElkanDistance().exec(input);
         final double expected = 0.15;
         assertEquals(expected, res, epsilon);
     }
